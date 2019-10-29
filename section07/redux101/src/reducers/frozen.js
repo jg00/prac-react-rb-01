b5c418575078aae0647eea730e181b8a7f8297e0
Jack Gamboa <jgamboa929@gmail.com>
2019-10-28 19:54:57 -0500

    redux store set up

--- section07/redux101/src/reducers/frozen.js
+++ section07/redux101/src/reducers/frozen.js
@@ -1,3 +1,19 @@
 export default (state = [], action) => {
-  return [];
+  return state;
 };
+
+/*
+  Reducer is just a function that returns state.
+
+  It takes two parameters:
+  1 Current state (usually provide default state)
+  2 Action - this will be an object that has type property and any payload
+*/
+
+/*
+  // Same as above
+  function frozen(state=[], action){
+    return state
+  }
+  export default frozen
+*/
--- section07/redux101/src/reducers/rootReducer.js
+++ section07/redux101/src/reducers/rootReducer.js
@@ -6,3 +6,18 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
+
+/*
+  This is the root reducer.  It is the store manager for al lthe reducers.
+
+  To make a root reducer:
+  1 Get a method from redux, called combineReducers which will combine individual reducers
+  into one big store.
+
+  2 Import each individual reducer
+
+  3 Call combineReducers() method and hand it an object.
+  Each key in combineReducers will be a piece of state in the redux store and
+  each value will be the value of that piece of state in the redux store
+  ex: combineReducers({frozen: frozenReducer})
+*/
