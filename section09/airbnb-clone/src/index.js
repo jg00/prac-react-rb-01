927f5f444bca471ae29443fe62d5b7197457e206
Jack Gamboa <jgamboa929@gmail.com>
2019-12-27 14:24:28 -0600

    redux-promise setup

--- section09/airbnb-clone/src/index.js
+++ section09/airbnb-clone/src/index.js
@@ -1,28 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+import reduxPromise from "redux-promise";
 
 import rootReducer from "./reducers/rootReducer";
 
-const store = createStore(
+const middleWareList = [reduxPromise];
+
+const theStore = applyMiddleware(...middleWareList)(createStore)(
   rootReducer /* preloadedState, */,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={theStore}>
     <App />
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
