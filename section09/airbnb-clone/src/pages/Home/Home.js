db0ec31a84d1dfdecadda4cc953829d120127525
Jack Gamboa <jgamboa929@gmail.com>
2019-12-29 07:46:40 -0600

    Get cities data, add spinner.

--- section09/airbnb-clone/src/pages/Home/Home.js
+++ section09/airbnb-clone/src/pages/Home/Home.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./Home.css";
 import SearchBox from "./SearchBox";
+import Spinner from "../../utility/Spinner/Spinner";
 
 class Home extends Component {
+  state = {
+    cities: []
+  };
+
+  async componentDidMount() {
+    const citiesUrl = `${window.apiHost}/cities/recommended`;
+    console.log(citiesUrl);
+    const recommendedCities = await axios.get(citiesUrl);
+    // console.log(recommendedCities.data);
+
+    this.setState({
+      cities: recommendedCities.data
+    });
+  }
+
   render() {
+    console.log(this.state.cities);
+
+    if (this.state.cities.length === 0) {
+      return <Spinner />;
+    }
+
     return (
       <div className="container-fluid">
         <div className="row">
--- section09/airbnb-clone/src/utility/Spinner/Spinner.js
+++ section09/airbnb-clone/src/utility/Spinner/Spinner.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
 import "./Spinner.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(faSpinner);
 
 class Spinner extends Component {
   render() {
-    return <div>Spinner</div>;
+    return (
+      <div className="spinner-wrapper">
+        <FontAwesomeIcon icon="spinner" size="6x" spin />
+      </div>
+    );
   }
 }
 
 export default Spinner;
+
+/*
+  npm i --save @fortawesome/fontawesome-svg-core
+  npm i --save @fortawesome/free-solid-svg-icons
+  npm i --save @fortawesome/react-fontawesome
+
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+  import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  library.add(faSpinner);
+*/
